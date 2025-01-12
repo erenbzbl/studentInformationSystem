0c1c14b2aa650d7bb684f09084c3d6dbc50279b0
erenbzbl <erenbozbel@gmail.com>
2025-01-12 18:06:10 +0300

    sayı tahmin oyunu

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,59 @@
+import java.util.Scanner;
+import java.util.ArrayList;
 import java.util.*;
-import java.util.Collections;
 
+//Java dilinde programın 0-100 arasında rastgele seçtiği bir sayıyı kullanıcının tahmin etmesini istediğimiz bir "Sayı Tahmin Oyunu" yapıyoruz.
 public class Main {
     public static void main(String[] args) {
-        Integer [] list;
-        Scanner scanner = new Scanner(System.in);
+            Random rand = new Random();
+            int number = rand.nextInt(100);
+            //int number = (int) (Math.random() * 100);
 
-        System.out.print("Dizi Boyutunu Giriniz: ");
-        int n = scanner.nextInt();
-        list = new Integer[n];
+        Scanner input = new Scanner(System.in);
+        int right = 0;
+        int selected;
+        int[] wrong = new int[5];
+        boolean isWin = false;
+        boolean isWrong = false;
 
+        System.out.println(number);
+        while (right < 5) {
+            System.out.print("Lütfen 1 ve 100 arasında bir tahmin giriniz: ");
+            selected = input.nextInt();
 
-        for (int i = 0; i < n; i++) {
-            System.out.print( i + ". Dizini Giriniz: ");
-            list[i] = scanner.nextInt();
+            if (selected < 0 || selected > 100) {
+                System.out.println("Lütfen geçerli bir sayı giriniz! ");
+                if (isWrong) {
+                    right++;
+                    System.out.println("Hatalı giriş yaptınız kalan hakkınız: " + (5 - right));
+                } else {
+                    isWrong = true;
+                    System.out.println("Her 2 hatalı girişte hakkınızdan bir düşer");
+                }
+                continue;
+            }
+            if (selected == number) {
+                System.out.println("Tebrikler Tahminini doğru! Tahmin ettiğin sayı: " + number);
+                isWin = true;
+                break;
+            } else {
+
+                if (selected > number) {
+                    System.out.println(selected + " Sayısı, gizli sayıdan büyüktür");
+                } else {
+                    System.out.println(selected + " Sayısı, gizli Sayıdan küçüktür");
+                }
+                wrong[right++] = selected;
+                System.out.println("Kalan hak: " + (5 - right));
+            }
         }
-        //Girilen dizi değerlerini küçükten büyüğe doğru sıralamaya yarayan "Arrays Komutu"
-        Arrays.sort(list);
-        //  Arrays.sort(list, Collections.reverseOrder()); Girilen dizi değerlerini büyükten küçüğe sıralar
 
-        System.out.println("Küçükten Büyüğe Sıralanmış Dizi: "  + Arrays.toString(list));
+        if (!isWin) {
+            System.out.println("Kaybettiniz");
+            if (!isWrong) {
+                System.out.println("Tahminleriniz: " + Arrays.toString(wrong));
+        }
+        }
 
     }
 }
