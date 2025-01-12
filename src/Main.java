3b1ec27ac99962ce114b974494ffde2f4f1b7543
erenbzbl <erenbozbel@gmail.com>
2025-01-12 23:27:38 +0300

    Palindrome

--- src/Main.java
+++ src/Main.java
@@ -2,58 +2,23 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.*;
 
-//Java dilinde programın 0-100 arasında rastgele seçtiği bir sayıyı kullanıcının tahmin etmesini istediğimiz bir "Sayı Tahmin Oyunu" yapıyoruz.
 public class Main {
+    static boolean isPalindrome(String str) {
+        int i = 0, j = str.length() - 1;
+        while (i < j) {
+            if (str.charAt(i) != str.charAt(j)) {
+                return false;
+            }
+                i++;
+                j--;
+        }
+        return true;
+    }
+
+
+
+
     public static void main(String[] args) {
-            Random rand = new Random();
-            int number = rand.nextInt(100);
-            //int number = (int) (Math.random() * 100);
-
-        Scanner input = new Scanner(System.in);
-        int right = 0;
-        int selected;
-        int[] wrong = new int[5];
-        boolean isWin = false;
-        boolean isWrong = false;
-
-        System.out.println(number);
-        while (right < 5) {
-            System.out.print("Lütfen 1 ve 100 arasında bir tahmin giriniz: ");
-            selected = input.nextInt();
-
-            if (selected < 0 || selected > 100) {
-                System.out.println("Lütfen geçerli bir sayı giriniz! ");
-                if (isWrong) {
-                    right++;
-                    System.out.println("Hatalı giriş yaptınız kalan hakkınız: " + (5 - right));
-                } else {
-                    isWrong = true;
-                    System.out.println("Her 2 hatalı girişte hakkınızdan bir düşer");
-                }
-                continue;
-            }
-            if (selected == number) {
-                System.out.println("Tebrikler Tahminini doğru! Tahmin ettiğin sayı: " + number);
-                isWin = true;
-                break;
-            } else {
-
-                if (selected > number) {
-                    System.out.println(selected + " Sayısı, gizli sayıdan büyüktür");
-                } else {
-                    System.out.println(selected + " Sayısı, gizli Sayıdan küçüktür");
-                }
-                wrong[right++] = selected;
-                System.out.println("Kalan hak: " + (5 - right));
-            }
-        }
-
-        if (!isWin) {
-            System.out.println("Kaybettiniz");
-            if (!isWrong) {
-                System.out.println("Tahminleriniz: " + Arrays.toString(wrong));
-        }
-        }
-
+        System.out.println(isPalindrome("eren"));
     }
 }
